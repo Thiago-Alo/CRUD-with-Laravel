6736399f9a995ecab89638159e2aa1dea4d9dde9
Thiago Alô <thiago_kadri@hotmail.com>
2022-05-16 11:44:35 +0100

    Finish Crud

---
+++ resources/views/master/footer.blade.php
@@ -0,0 +1,9 @@
+<div class="container">
+    <div class="row">
+        <div class="col-12">
+            <p class="text-center">
+                by Thiago Alô
+            </p>
+        </div>
+    </div>
+</div>
